4256c5a1cf1979004e218541d0d1336ab845b49d
Mikdad Mohammad Asif <mikdadmohammadasif@Mikdads-MacBook-Air.local>
2023-01-08 12:03:59 +0600

    update

---
+++ variable_in_python.py
@@ -0,0 +1,17 @@
+#integer variable in python
+
+number1 = 5000
+number2 = 3000
+
+print(number1, " ", number2)
+
+#String type variable
+my_name = "Mikdad Mohammad Asif"
+
+print(my_name)
+
+#Boolean type variable in python
+have_money = True
+have_property = False
+
+print(have_money)
