78d5f6e41cbbd2ffa4ff0a173bfc5ea7b1f88423
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2023-01-19 19:14:41 +0600

    update

--- check_date_type.py
+++ check_date_type.py
@@ -1,3 +1,15 @@
 price = 12
 price_type = type(price)
 print(price_type)
+
+name = "Mikdad"
+name_type = type(name)
+print(name_type)
+
+is_hot = True
+is_hot_type = type(is_hot)
+print(is_hot_type)
+
+#another way to check type of data
+
+print(type(is_hot))
