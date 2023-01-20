ca3d57a96c822c8387e0107519bb530c5ea650fe
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2023-01-21 00:36:39 +0600

    update

---
+++ input.py
@@ -0,0 +1,16 @@
+age = input("Tell me your age: ")
+# print(age)
+#NB: python initially take all input as a string
+
+num1 = input("First number is: ")
+num2 = input("Second number is: ")
+
+num1_int = int(num1)
+num2_int = int(num2)
+
+result = num1_int + num2_int
+
+print(result)
+
+final = f"the result is {result}"
+print(final)
