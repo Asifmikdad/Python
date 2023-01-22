be2457ad1a83a9177077592c693b922adb29c656
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2023-01-22 16:54:46 +0600

    update

---
+++ comparison.py
@@ -0,0 +1,11 @@
+apple_price = 40
+orange_price = 60
+
+print (apple_price < orange_price)
+print (apple_price > orange_price)
+print (apple_price <= orange_price)
+print (apple_price >= orange_price)
+print (apple_price == orange_price)
+print (apple_price != orange_price)
+
+#there are 6 type of comparison 
