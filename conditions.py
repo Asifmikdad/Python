7cb820236fae7cca6aa8c6f80f797da4b712b53c
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2023-01-22 20:39:05 +0600

    update

---
+++ conditions.py
@@ -0,0 +1,8 @@
+meal_price = 150
+
+if meal_price > 200:
+    print("I can effort food")
+elif meal_price ==150:
+    print("I can eat that meal")
+else:
+    print("I can't effort food")
---
+++ nested_condition.py
@@ -0,0 +1,9 @@
+salary = 50000
+has_flat = True
+
+if salary > 3500:
+    if has_flat == True:
+        print("You can get married")
+    else:
+        print("You can't get married")
+print("Complete")
