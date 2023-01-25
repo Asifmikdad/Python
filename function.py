64e56936ce2d79c1e78f3666faf1215aa60e9889
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2023-01-25 14:51:00 +0600

    update

---
+++ function.py
@@ -0,0 +1,25 @@
+# def means defining a function in python
+
+# example 1
+def double_it(num):
+    result = num *2
+    print(result)
+
+double_it(4)
+
+#example 2
+
+def sum(num1, num2):
+    summation = num1 + num2
+    print(summation)
+
+sum(5,6)
+
+# how to use return in python using function
+
+def multiply(num1, num2):
+    mul = num1 * num2
+    return mul
+
+output = multiply(4,5)
+print(output)
